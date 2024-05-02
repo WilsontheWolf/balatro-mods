0ad78c42b10b4e8fe21a31757a7a7696c49137cf
WilsontheWolf <33164598+WilsontheWolf@users.noreply.github.com>
2024-05-02 16:20:07 -0600

    feat: no background

---
+++ steamodded/NoBackground/NoBackground.lua
@@ -0,0 +1,16 @@
+--- STEAMODDED HEADER
+--- MOD_NAME: No Background
+--- MOD_ID: nobackground
+--- MOD_AUTHOR: [WilsontheWolf]
+--- MOD_DESCRIPTION: Disables the background on start.
+
+----------------------------------------------
+------------MOD CODE -------------------------
+
+function SMODS.INIT.nobackground()
+    G.debug_background_toggle = true
+end
+
+----------------------------------------------
+------------MOD CODE END----------------------
+
