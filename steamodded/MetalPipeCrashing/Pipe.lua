7807aa8272a849e7a8f7f05dd3eda49ed55441a5
WilsontheWolf <33164598+WilsontheWolf@users.noreply.github.com>
2024-09-22 17:08:06 -0600

    metal pipe crash noise

---
+++ steamodded/MetalPipeCrashing/Pipe.lua
@@ -0,0 +1,14 @@
+--- STEAMODDED HEADER
+--- MOD_NAME: Metal Pipe Crashing Noise
+--- MOD_ID: PipeCrash
+--- MOD_AUTHOR: [WilsontheWolf]
+--- MOD_DESCRIPTION: THUD, CLANG
+--- VERSION: 1.0
+--- PREFIX: PipeCrash
+--- DEPENDENCIES: [Steamodded>=1.0.0~ALPHA-0919a]
+
+pipe_crash_sound = SMODS.Sound({
+    key = "pipe_crash_sound",
+    path = "pipe.ogg"
+})
+
